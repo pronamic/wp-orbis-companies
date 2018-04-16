ef7ac610f8c573fcb4bf91af668248d46e1bc7de
Ruben Droogh <rubendroogh@iMac-van-Leo-2.local>
2018-04-16 16:16:19 +0200

    Vertalingen.

--- admin/meta-box-company-details.php
+++ admin/meta-box-company-details.php
@@ -92,7 +92,7 @@ wp_nonce_field( 'orbis_save_company_details', 'orbis_company_details_meta_box_no
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label for="orbis_iban"><?php _e( 'IBAN', 'orbis' ); ?></label>
+				<label for="orbis_iban"><?php _e( 'IBAN', 'orbis-companies' ); ?></label>
 			</th>
 			<td>
 				<input type="text" id="orbis_iban" name="_orbis_iban" value="<?php echo esc_attr( $iban ); ?>" class="regular-text" />
