a863ba2d38ab8a42530ef2a0b2598fa185abaf4f
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-30 14:58:52 +0100

    Update orbis-companies.php

--- orbis-companies.php
+++ orbis-companies.php
@@ -1,22 +1,30 @@
 <?php
-/*
-Plugin Name: Orbis Companies
-Plugin URI: https://www.pronamic.eu/plugins/orbis-companies/
-Description: The Orbis Companies plugin extends your Orbis environment with the option to manage companies.
+/**
+ * Orbis Companies
+ *
+ * @package   Pronamic\Orbis\Companies
+ * @author    Pronamic
+ * @copyright 2024 Pronamic
+ * @license   GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Orbis Companies
+ * Plugin URI:        https://wp.pronamic.directory/plugins/orbis-companies/
+ * Description:       The Orbis Companies plugin extends your Orbis environment with the option to manage companies.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pronamic
+ * Author URI:        https://www.pronamic.eu/
+ * Text Domain:       orbis-companies
+ * Domain Path:       /languages/
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wp.pronamic.directory/plugins/orbis-companies/
+ * GitHub URI:        https://github.com/pronamic/wp-orbis-companies
+ */
 
-Version: 1.0.0
-Requires at least: 3.5
-
-Author: Pronamic
-Author URI: https://www.pronamic.eu/
-
-Text Domain: orbis-companies
-Domain Path: /languages/
-
-License: Copyright (c) Pronamic
-
-GitHub URI: https://github.com/wp-orbis/wp-orbis-companies
-*/
+namespace Pronamic\Orbis\Companies;
 
 /**
  * Autoload.
@@ -26,10 +34,13 @@ require_once __DIR__ . '/vendor/autoload_packages.php';
 /**
  * Bootstrap.
  */
-function orbis_companies_bootstrap() {
-	global $orbis_companies_plugin;
+\add_action(
+	'plugins_loaded',
+	function () {
+		\load_plugin_textdomain( 'orbis-companies', false, \dirname( \plugin_basename( __FILE__ ) ) . '/languages' );
 
-	$orbis_companies_plugin = new Orbis_Companies_Plugin( __FILE__ );
-}
+		global $orbis_companies_plugin;
 
-add_action( 'plugins_loaded', 'orbis_companies_bootstrap' );
+		$orbis_companies_plugin = new Orbis_Companies_Plugin( __FILE__ );
+	}
+);
